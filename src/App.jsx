4a05f79207575554c8fdded1cb39b436c320db83
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-09-23 11:54:35 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
